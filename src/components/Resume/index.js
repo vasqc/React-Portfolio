436834c0c0681f366d44bd38d25dba9fc967e8c1
vasqc <90007988+vasqc@users.noreply.github.com>
2022-03-22 17:21:15 -0400

    better and changed code, including images and hopefully some functionality

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
 function Resume() {
   return (
     <section className="my-5">
       <div className="my-2">
-      <p>
-        Download my <a href="https://www.linkedin.com/in/christian-vasquez-0a7634164/">resume</a>
+        <p>
+          Download my{" "}
+          <a href="https://www.linkedin.com/in/christian-vasquez-0a7634164/">
+            resume
+          </a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
+          <li>React</li>
+          <li>Bootstrap</li>
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>React</li>
-          <li>Bootstrap</li>
         </ul>
         <h3>Back-end Proficiencies</h3>
         <ul className="skills">
-          <li>APIs</li>
-          <li>Node</li>
+          <li>NPM (NODE)</li>
           <li>Express</li>
           <li>MySQL, Sequelize</li>
           <li>MongoDB, Mongoose</li>
-          <li>REST</li>
           <li>GraphQL</li>
         </ul>
       </div>
