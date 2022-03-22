668dae19ee01c8424256b6dcf867792b1dbe1b32
vasqc <90007988+vasqc@users.noreply.github.com>
2022-03-22 16:59:45 -0400

    render to correct page for the react dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
+
+
+//ReactDom make sure you pull and render on this page.
 ReactDOM.render(
   <React.StrictMode>
     <App />
