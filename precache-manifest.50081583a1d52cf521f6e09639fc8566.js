c8a04b80026e5d47d9e6999e110452b85cd32546
vasqc <90007988+vasqc@users.noreply.github.com>
2022-03-25 15:05:52 -0400

    Updates

---
+++ precache-manifest.50081583a1d52cf521f6e09639fc8566.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab2417ee97327e86d0f4eba3e70373e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf3d19fd766c38d321a",
+    "url": "/static/css/main.c1b98356.chunk.css"
+  },
+  {
+    "revision": "7e06136b62c3a3efbcc1",
+    "url": "/static/js/2.6a90447d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6a90447d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf3d19fd766c38d321a",
+    "url": "/static/js/main.90ccbf1b.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "d10f53eda898df94099154ec884eeb4f",
+    "url": "/static/media/coding.d10f53ed.png"
+  }
+]);
